f6cb631014fe2b398915c17ccaee1ce60b661e37
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-10-09 16:41:25 -0300

    Funcionalidade: filtragem e paginacao de chamadas

--- src/main/resources/static/lib/main.js
+++ src/main/resources/static/lib/main.js
@@ -1,118 +1,105 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('Hello Bulma!');
+function searchChamada() {
+    let searchInput = document.querySelector('#input-search');
 
-  var notification = document.querySelector('[data-notification]');
+    if(searchInput) {
+        var searchString = '';
+        var page = 0;
+        requestChamadaService(searchString, page);
 
-  if (notification) {
-    var notificationbody = document.querySelector('.column .notification').parentNode;
-    console.log(notificationbody);
+        searchInput.addEventListener('input', () => {
+            searchString = searchInput.value;
+            requestChamadaService(searchString, 0);
+        });
 
-    setTimeout(function () {
-      notificationbody.parentNode.removeChild(notificationbody);
-    }, 5000);
-  }
-});
+        function requestChamadaService(searchString, page) {
+            let url = 'http://localhost:8080/chamadas/?titulo=' + searchString + '&pagina=' + page;
 
-document.addEventListener('DOMContentLoaded', function () {
-  (document.querySelectorAll('.notification .delete') || []).forEach(function ($delete) {
-    var $notification = $delete.parentNode;
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200)
+                        updateTable(JSON.parse(xhr.responseText));
 
-    $delete.addEventListener('click', function () {
-      $notification.parentNode.removeChild($notification);
+                        let pageLink = document.querySelectorAll('.pagination-link');
+                        for (let i = 0; i < pageLink.length; i++) {
+                            pageLink[i].addEventListener('click', () => {
+                                page = i
+                                requestChamadaService(searchString, page);
+                            })
+                        }
+                    }
+                }
+                xhr.send();
+        }
+
+        function updateTable(data) {
+            let listaChamadas = data.content;
+            let tbody = document.querySelector('#table-content');
+            let paginationList = document.querySelector('.pagination-list');
+            var urlId = '';
+            tbody.innerHTML = '';
+            paginationList.innerHTML = '';
+
+            if (listaChamadas.length  == 0) {
+                tbody.innerHTML = `<p>Nada encontrado.</p>`
+            } else {
+                listaChamadas.map(chamada => {
+                    urlId = `/chamadas/${chamada.id}`;
+                    let row = `<tr>
+                        <td><a href="${urlId}" class="has-text-weight-medium">${chamada.titulo}</a></td>
+                        <td><span class="${chamada.status !== 'FECHADO' ? 'tag is-spaced is-rounded is-primary' : 'tag is-spaced is-rounded is-black'}">${chamada.status}</span></td>
+                        <td><strong>${chamada.dataInicio}</strong> até <strong>${chamada.dataTermino}</strong>
+                        <td>41/50</td>
+                        <td><a href="${urlId}" class="has-text-weight-medium">Acessar chamada</a></td>
+                    </tr>`
+                    tbody.innerHTML += row;
+                })
+
+                for(let i = 0; i < data.totalPages; i++) {
+                    let pageButton = `
+                        <li><a class="${data.pageable.pageNumber == i ?  'pagination-link is-current' : 'pagination-link'}" aria-label="Página 1" aria-current="page">${i}</a></li>
+                    `;
+                    paginationList.innerHTML += pageButton;
+                }
+            }
+        }
+    }
+}
+
+function loadDOM() {
+    console.log('Hello Bulma!');
+
+    document.addEventListener('DOMContentLoaded', function () {
+      var notification = document.querySelector('[data-notification]');
+
+      if (notification) {
+        var notificationbody = document.querySelector('.column .notification').parentNode;
+        console.log(notificationbody);
+
+        setTimeout(function () {
+          notificationbody.parentNode.removeChild(notificationbody);
+        }, 5000);
+      }
     });
-  });
-});
-
-document.querySelector('.detalhes-opcoes-botao').addEventListener('click', function () {
-  var bodyOptions = document.querySelector('.detalhes-opcoes-body');
-  bodyOptions.style.display == 'block' ? bodyOptions.style.display = 'none' : bodyOptions.style.display = 'block';
-});
-
-var currentTab = 0;
-showTab(currentTab);
-
-document.querySelector(".form-navigation-next").addEventListener("click", function () {
-  return nextPrev(1);
-});
-
-document.querySelector(".form-navigation-back").addEventListener("click", function () {
-  return nextPrev(-1);
-});
-
-function showTab(tabWillBeDisplayed) {
-  var tabs = document.getElementsByClassName("tab");
-  tabs[tabWillBeDisplayed].style.display = "block";
-
-  if (tabWillBeDisplayed == 0) {
-    document.querySelector(".form-navigation-back").style.visibility = "hidden";
-  } else {
-    document.querySelector(".form-navigation-back").style.visibility = "visible";
-  }
-
-  if (tabWillBeDisplayed == tabs.length - 1) {
-    document.querySelector(".form-navigation-next").innerHTML = "Finalizar";
-  } else {
-    document.querySelector(".form-navigation-next").innerHTML = "Próximo";
-  }
-
-  fixStepIndicator(tabWillBeDisplayed);
 }
 
-function nextPrev(tabWillBeDisplayed) {
-  var tabs = document.getElementsByClassName("tab");
-  tabs[currentTab].style.display = "none";
-  currentTab = currentTab + tabWillBeDisplayed;
-
-  if (currentTab >= tabs.length) {
-    document.getElementById("regForm").submit();
-    return false;
-  }
-
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-  showTab(currentTab);
-}
-
-function validateForm() {
-  // This function deals with validation of the form fields
-  var x,
-      y,
-      i,
-      valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      y[i].className += " invalid";
-      // and set the current valid status to false:
-      valid = false;
+function detailsOption() {
+    let botaoOpcao = document.querySelector('.detalhes-opcoes-botao');
+    if (botaoOpcao) {
+        botaoOpcao.addEventListener('click', function () {
+          var bodyOptions = document.querySelector('.detalhes-opcoes-body');
+          bodyOptions.style.display == 'block' ? bodyOptions.style.display = 'none' : bodyOptions.style.display = 'block';
+        });
     }
-  }
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-  }
-  return valid; // return the valid status
 }
 
-function fixStepIndicator(tabWillBeDisplayed) {
-  var tabs = document.getElementsByClassName("step-info-section");
-
-  for (var i = 0; i < tabs.length; i++) {
-    if (i > tabWillBeDisplayed) {
-      tabs[i].classList.remove("step-info-checked");
-    } else if (i < tabWillBeDisplayed) {
-      tabs[i].classList.add("step-info-checked");
-    }
-  }
-
-  tabs[tabWillBeDisplayed].classList.add("step-info-checked");
+function main() {
+    loadDOM();
+    searchChamada();
+    detailsOption();
 }
+
+main();
