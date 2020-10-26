1525c8f1adfb32d126a9d3a05a6a344bada92643
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-10-26 11:15:39 -0300

    refatoracao front: datatables em portugues

--- src/main/resources/static/lib/main.js
+++ src/main/resources/static/lib/main.js
@@ -16,6 +16,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
 /* harmony export */ });
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -30,28 +36,33 @@ var DataTablesListFilter = /*#__PURE__*/function () {
   _createClass(DataTablesListFilter, null, [{
     key: "bind",
     value: function bind() {
+      var messages = {
+        search: "",
+        searchPlaceholder: "Procurar paciente",
+        info: "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+        emptyTable: "Nenhum dado disponível na tabela",
+        infoEmpty: "Mostrando de 0 até 0 de 0 registros",
+        infoFiltered: "(Filtrado de _MAX_ registros)",
+        thousands: ".",
+        lengthMenu: "Mostrando _MENU_ registros",
+        loadingRecords: "Carregando...",
+        paginate: {
+          first: "Primeiro",
+          last: "Último",
+          next: "Próximo",
+          previous: "Voltar"
+        }
+      };
       $("#pacientes").dataTable({
-        language: {
-          search: "",
-          searchPlaceholder: "Procurar paciente",
-          info: "Mostrando de _START_ até _END_ de _TOTAL_ registros"
-        },
+        language: _objectSpread({}, messages),
         dom: ' <"top"f>rt<"bottom"ip><"clear">'
       });
       $("#cuidadores").dataTable({
-        language: {
-          search: "",
-          searchPlaceholder: "Procurar cuidador",
-          info: "Mostrando de _START_ até _END_ de _TOTAL_ registros"
-        },
+        language: _objectSpread({}, messages),
         dom: ' <"top"f>rt<"bottom"ip><"clear">'
       });
       $("#chamadas").dataTable({
-        language: {
-          search: "",
-          searchPlaceholder: "Procurar chamada",
-          info: "Mostrando de _START_ até _END_ de _TOTAL_ registros"
-        },
+        language: _objectSpread({}, messages),
         dom: ' <"top"f>rt<"bottom"ip><"clear">'
       });
     }
